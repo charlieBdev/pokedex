addc22590497de9a829e5aa19436bc29c06fd263
Charlie B <67501435+charlieBdev@users.noreply.github.com>
2024-01-31 19:04:05 +0000

    Update App.jsx colour

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 	const [gameStarted, setGameStarted] = useState(false);
 
 	return (
-		<div className='bg-gradient-to-br from-[#FFDE00] via-[#ffdd00ab] to-[#FFDE00] h-screen font-mono text-neutral-950 flex flex-col gap-3 items-center justify-between pt-24'>
+		<div className='bg-gradient-to-br from-yellow-400 via-yellow-200 to-yellow-400 h-screen font-mono text-neutral-950 flex flex-col gap-3 items-center justify-between pt-24'>
 			<div>
 				<Header />
 				<PokemonList
