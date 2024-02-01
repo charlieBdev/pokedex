ec52b4b2f9ff39d33c2c0231c36d64f2d345bbc6
Charlie B <67501435+charlieBdev@users.noreply.github.com>
2024-02-01 07:00:06 +0000

    Update Footer.jsx
    Change text and use footer tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 const Footer = () => {
 	return (
-		<p className='fixed bottom-1 right-1 text-xs text-center p-3'>
+		<footer className='fixed bottom-1 right-1 text-xs text-center p-3'>
 			Made by{' '}
 			<a
 				href='https://charliebdev.vercel.app/'
 				target='_blank'
 				rel='noreferrer'
 			>
-				charliebdev
+				charlieb
 			</a>{' '}with 🙏 to PokeAPI
-		</p>
+		</footer>
 	);
 };
 
